9e7d48d12fb1d62e57146429acc11abaab5b3b55
f136pix <73194798+f136pix@users.noreply.github.com>
2022-05-03 20:41:21 -0300

    Add files via upload

---
+++ palindromo.js
@@ -0,0 +1,27 @@
+
+
+
+
+var descobrePalindromo = function(head) {
+    let esquerda = [head[0],head.length/2]
+    let direita = [(head.length / 2) +1,head.length]
+    let cont =0
+    let resEsquerda =[]
+    let resDireita =[]
+
+    if (esquerda.length == direita.length){
+        for(cont=0;cont<esquerda.length;cont++){
+            resEsquerda.push(esquerda[cont])
+            resDireita.push(direita[cont])
+            cont++
+        }
+    }
+    for(atual=0;atual<resEsquerda;atual++){
+        if (resEsquerda[atual] != resDireita[atual]){
+            return false
+        }
+    }
+    return true
+};
+
+
