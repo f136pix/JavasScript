490dae57f0499265671b72a44bf33dff8e76224d
f136pix <73194798+f136pix@users.noreply.github.com>
2022-05-03 21:14:19 -0300

    Add files via upload

---
+++ descobreElementos.js
@@ -0,0 +1,16 @@
+var descobreNumeros= function(nums, target) {
+    for(n1=0; n1 < nums.length; n1++ ){
+        
+        for(n2=0; n2 < nums.length; n2++){
+            
+        if(n1 == n2){
+        continue
+         }
+        else if(nums[n1] + nums[n2] == target)
+            return([n1,n2])
+         }
+            
+    }
+             
+};
+    
