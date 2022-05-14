032939445309ac635751b9884d6fa9b2b33231b8
f136pix <73194798+f136pix@users.noreply.github.com>
2022-05-13 23:23:46 -0300

    Add files via upload

---
+++ numerosSequencia.js
@@ -0,0 +1,19 @@
+function letterCombinations(digits) {
+    let possibleCombinations = []
+    const letters = ["abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+    let digit1 = letters[digits[0] -2]
+    let digit2 = letters[digits[1] -2]
+    
+    for(i=0; i<digit1.length; i++){
+        for(o=0; o<digit2.length; o++){
+    
+        possibleCombinations.push(digit1[i].concat(digit2[o]))         
+        
+        }
+    }
+return possibleCombinations
+    
+}
+
+
+
