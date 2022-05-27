f6437b47c9c04a88fc22267132d1f0e3b5aa2207
Filipe Furlan <73194798+f136pix@users.noreply.github.com>
2022-05-27 18:29:41 -0300

    Add files via upload

---
+++ hashtag.js
@@ -0,0 +1,14 @@
+//A partir de uma frase recebida em forma de string,a função ira formatar o texto retirando
+//os espaços, colocando e letras após os espaços maiusculas e adicionando uma hashtag no começo.
+
+function hashtag(frase) {
+    let fraseSemEspaco = frase.trim().split(" ")
+    let fraseFormatada = []
+    for (i = 0; i < fraseSemEspaco.length; i++) {
+        fraseFormatada[i] = fraseSemEspaco[i][0].toUpperCase() + fraseSemEspaco[i].toLowerCase().slice(1)
+    }
+    fraseFormatada.unshift('#')
+    fraseFormatada = fraseFormatada.join('')
+    console.log(fraseFormatada)
+    return fraseFormatada
+}
